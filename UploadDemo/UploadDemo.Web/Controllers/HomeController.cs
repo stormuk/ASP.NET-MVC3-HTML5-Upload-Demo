fa4de5763e9399bea325fc8d6a45fefbbba2e5bd
Adam Pope <adam@storm-consultancy.com>
2011-12-30 14:38:24 +0000

    Update to finish the demo

---
+++ UploadDemo/UploadDemo.Web/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadDemo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase myfile)
+        {
+
+            var foo = myfile;
+
+            if (myfile.ContentLength > 0)
+            {
+                // Save file
+                var fileName = myfile.FileName;
+                var path = System.IO.Path.Combine(Server.MapPath("~/Uploads"), fileName);
+                myfile.SaveAs(path);
+
+                return Content("ok");
+            }
+
+            return HttpNotFound();
+        }
+    }
+}
